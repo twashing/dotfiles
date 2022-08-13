860b37e0e7ee0b2afa928a2ea6cca114bfea3844
Timothy Washington <twashing@gmail.com>
2022-08-13 19:46:15 -0400

    Turning on Doom modules Adding Font configuration Adding local package configurations

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Timothy Washington"
+      user-mail-address "twashing@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -21,6 +21,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+(setq doom-font (font-spec :family "PragmataPro" :size 16 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "PragmataPro" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -45,7 +48,126 @@
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
+
+
+;; General
+(map! "M-<backspace>" #'sp-backward-kill-word)
+(map! "C-c C-k" #'eval-buffer)
+(map! "C-c M-c" #'upcase-word)
+(map! "C-x M-x" #'isearch-forward-symbol-at-point)
+(map! "C-x RET" #'magit-status)
+(map! "M-W" #'whitespace-cleanup)
+
+
+;; Swiper
+(map! "M-m s s" #'swiper)
+(map! "M-m s S" #'swiper-isearch-thing-at-point)
+(map! "M-m s B" #'swiper-all)  ;; search all buffers
+
+
+;; Projectile
+(map! "M-m p p" #'projectile-switch-project)
+(map! "M-m p f" #'projectile-find-file)
+(map! "M-m p r" #'projectile-replace
+(map! "M-m p R" #'projectile-replace-regexp)
+(map! "M-m p S" #'projectile-save-project-buffers)
+
+
+;; Avy
+(map! "C-c g c" #'avy-goto-char-2)
+
+
+;; Navigation
+;;(fset 'buf-move-up "\C-u10\C-p")
+;;(fset 'buf-move-down "\C-u10\C-n")
+(use-package! buffer-move
+  :config
+  (map! "\C-u10\C-p" #'buf-move-up)
+  (map! "\C-u10\C-n" #'buf-move-down))
+
+(map! "C-d" #'sp-kill-sexp)
+(map! "C-M-k" #'copy-sexp-at-point)
+(map! "M-[" #'ace-select-window)
+(map! "C-c M-[" #'ace-swap-window)
+(map! "C-x M-[" #'ace-delete-window)
+(map! "M-y" #'browse-kill-ring)
+(map! "C-M-SPC" #'delete-whitespace-except-one)
+(map! "C-M-[" #'scroll-other-window-down)
+(map! "C-M-]" #'scroll-other-window)
+(map! "C-M-s" #'sp-splice-sexp)
+
+
+;; Smartparens Navigation
+(map! "C-M-u" #'sp-up-sexp)
+(map! "M-u" #'sp-backward-up-sexp)
+
+(map! "C-M-d" #'sp-down-sexp)
+(map! "M-d" #'sp-backward-down-sexp)
+
+(map! "C-M-j" #'sp-forward-slurp-sexp)
+(map! "C-x C-M-j" #'sp-forward-barf-sexp)
+
+(map! "C-M-y" #'sp-backward-slurp-sexp)
+(map! "C-x C-M-y" #'sp-backward-barf-sexp)
+
+(map! "C-M-n" #'sp-next-sexp)
+(map! "M-r" #'sp-raise-sexp)
+
+;; Multiple cursors
+(map! "C-c m n l" #'mc/mark-next-lines)
+(map! "C-c m n t" #'mc/mark-next-like-this)
+
+(map! "C-c m n w" #'mc/mark-next-like-this-word)
+(map! "C-c m n W" #'mc/mark-next-word-like-this)
+
+(map! "C-c m n s" #'mc/mark-next-like-this-symbol)
+(map! "C-c m n S" #'mc/mark-next-symbol-like-this)
+
+(map! "C-c s n" #'mc/skip-to-next-like-this)
+(map! "C-c s p" #'mc/skip-to-previous-like-this)
+(map! "C-c m i n" #'mc/insert-numbers)
+
+(map! "C-c m p l" #'mc/mark-previous-lines)
+(map! "C-c m a t" #'mc/mark-all-like-this)
+(map! "C-c m a w" #'mc/mark-all-words-like-this)
+(map! "C-c m a s" #'mc/mark-all-symbols-like-this)
+(map! "C-c m a r" #'mc/mark-all-in-region)
+(map! "C-c m a x" #'mc/mark-all-in-region-regexp)
+(map! "C-c m a d" #'mc/mark-all-like-this-dwim)
+(map! "C-c m a D" #'mc/mark-all-dwim)
+
+(map! "C-c m e l" #'mc/edit-lines)
+(map! "C-c m e b" #'mc/edit-beginnings-of-lines)
+(map! "C-c m e e" #'mc/edit-ends-of-lines)
+
+
+;; Hide / Show
+(map! "C-o" #'hs-toggle-hiding)
+(map! "C-c @ C-M-h" #'hs-hide-all)
+(map! "C-c @ C-M-s" #'hs-show-all)
+
+
+;; Miscellaneous
+;; To get information about any of these functions/macros, move the cursor over
+(map! "C-L" #'+default/newline-above)
+(map! "C-l" #'+default/newline-below)
+
+
+;; Copy line
+(defun copy-line (&optional arg)
+  "Do a kill-line but copy rather than kill.  This function directly calls
+  kill-line, so see documentation of kill-line for how to use it including prefix
+  argument and relevant variables.  This function works by temporarily making the
+  buffer read-only."
+  (interactive "P")
+  (let ((buffer-read-only t)
+        (kill-read-only-ok t))
+    (kill-line arg)))
+
+(map! "C-c k" #'copy-line)
+(map! "C-c K" #'avy-copy-line)
+
+
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
