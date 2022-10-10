4ea9895124551d6b6c7cf4c9a01872c51c53fe64
Timothy Washington <timothy.washington@walmart.com>
2022-10-10 10:46:52 -0400

    Adding Roam Markdown configuration

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(corfu))
- '(warning-suppress-types '((org-element-cache) (org-element-cache) (defvaralias))))
+ '(safe-local-variable-values
+   '((org-roam-directory . "/Users/vn545yy/Projects/eReceipts-core/docs")))
+ '(warning-suppress-types '((org-element-cache) (org-element-cache) (defvaralias)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
