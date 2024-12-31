63e4449e7e9ba95b2477f0a78c403d7f0269655e
Timoty Washington <twashing@gmail.com>
2024-12-31 15:17:18 -0500

    Adding sudo qualifiers before each docker invocation

--- run-config.sh
+++ run-config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 image_path="$(guix system image --network --image-type=docker $1)"
-image_id="$(docker load < $image_path | cut -f3 -d ' ')"
+image_id="$(sudo docker load < $image_path | cut -f3 -d ' ')"
 
-# docker run -ti $image_id /run/current-system/profile/bin/bash --login
+# sudo docker run -ti $image_id /run/current-system/profile/bin/bash --login
 
-container_id="$(docker create -p 8080:8080/tcp --privileged $image_id)"
-docker start $container_id
-docker exec -ti $container_id /run/current-system/profile/bin/bash --login
+container_id="$(sudo docker create -p 8080:8080/tcp --privileged $image_id)"
+sudo docker start $container_id
+sudo docker exec -ti $container_id /run/current-system/profile/bin/bash --login
